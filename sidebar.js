79d7912c24b2ef319de985ff46c95042f30bb023
Jack <melcher.jack@gmail.com>
2020-03-21 02:48:38 -0700

    Updated logo image and guides page. Moved blank images to separate folder. Fixed some missing images.

--- sidebar.js
+++ sidebar.js
@@ -1,16 +1,6 @@
 var side = document.getElementsByClassName("sidenav")[0];
 var sidebutton = document.getElementsByClassName("sidenavbutton")[0];
-function ToggleSide()
-{
-    if(side.style.display == "none" || side.style.display == "")
-    {
-        side.style.display = "block";
-    }
-    else
-    {
-        side.style.display = "none";
-    }
-}
+var menuicon = document.getElementsByClassName("menuicon")[0];
 
 var mq = window.matchMedia("(max-width: 768px)");
 function resizeSideNav(mq)
@@ -24,6 +14,25 @@ function resizeSideNav(mq)
     {
         side.style.display = "block";
         sidebutton.style.display = "none";
+        menuicon.classList.replace("fa-times","fa-bars");
+
     }
 }
 mq.addListener(resizeSideNav);
+
+function ToggleSide()
+{
+    if(mq.matches)
+    {
+        if(side.style.display == "none" || side.style.display == "")
+        {
+            side.style.display = "block";
+            menuicon.classList.replace("fa-bars","fa-times");
+        }
+        else
+        {
+            side.style.display = "none";
+            menuicon.classList.replace("fa-times","fa-bars");
+        }
+    }
+}
