9feaa0a7aec64984a187a0968ddf8e362ec0372e
Jack <melcher.jack@gmail.com>
2020-03-26 05:22:59 -0700

    Made Sidenav more maintainable by making it a js function. Removed section fixed on all pages.

--- sidebar.js
+++ sidebar.js
@@ -36,3 +36,34 @@ function ToggleSide()
         }
     }
 }
+
+function makeSidebar(){
+    //console.log("Making Sidebar");
+    
+    var sidenav = document.getElementsByClassName("sidenav")[0];
+    var divContainer;
+
+    var pagenames =  ["beginner","industry","ip","pr","youtube","kickstarter"];
+    var pagetitle = ["Beginner's Guide","The Computer and Video Game Industry","The Strength of Intellectual Property","Public Relations and Marketing","Building a Successful YouTube Channel","How to be successful at Kickstarter"];
+
+    var beginnerSections = ["Overview","Start Small","Concepting","Managing Scope","Writing a Game Design Doc","Tools and Assets FAQs","How Experience affects Development","Recruiting Team Members","Production and Task Management","Cost of Development","Funding Options","Making a Business Plan","Marketing","Post Mortem"]
+    var industrySections = ["Overview and Highlights of the Industry","Getting a Job in the Games Industry","Working in Game Testing","The Downside of Game Testing","Departments in a AAA Studio","Culture in the Game Industry","Unionization Efforts in the Industry"];
+
+    var i;
+    for(var j = 0; j < pagenames.length; j++)
+    {
+        divContainer = document.createElement("div");
+        i = document.createElement("a");
+        i.href = "guides/"+pagenames[j]+".html";
+        i.innerHTML = pagetitle[j];
+        i.classList.add("bold");
+        //console.log(location.href.split("/").slice(-1));
+        if(location.href.split("/").slice(-1) == pagenames[j]+".html"){
+            i.classList.add("underline");
+        }
+        divContainer.appendChild(i);
+        divContainer.appendChild(document.createElement("br"));
+        divContainer.appendChild(document.createElement("br"));
+        sidenav.appendChild(divContainer);
+    }
+}
