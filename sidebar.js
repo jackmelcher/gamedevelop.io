0acfc5ad78622d3c99afdeb7ba495e2ac1a4d95a
Jack <melcher.jack@gmail.com>
2020-03-27 04:47:29 -0700

    Made new javascript to move scrollbar appropriately to anchored ids.

--- sidebar.js
+++ sidebar.js
@@ -47,6 +47,8 @@ function makeSidebar(){
     var pagetitle = ["Beginner's Guide","The Computer and Video Game Industry","The Strength of Intellectual Property","Public Relations and Marketing","Building a Successful YouTube Channel","How to be successful at Kickstarter"];
 
     var beginnerSections = ["Overview","Start Small","Concepting","Managing Scope","Writing a Game Design Doc","Tools and Assets FAQs","How Experience affects Development","Recruiting Team Members","Production and Task Management","Cost of Development","Funding Options","Making a Business Plan","Marketing","Post Mortem"]
+    var beginnerids = ["overview","startsmall","concept","scope","gdd","tools","experience","team","production","cost","funding","business","marketing","postmortem"]
+
     var industrySections = ["Overview and Highlights of the Industry","Getting a Job in the Games Industry","Working in Game Testing","The Downside of Game Testing","Departments in a AAA Studio","Culture in the Game Industry","Unionization Efforts in the Industry"];
 
     var i;
@@ -58,12 +60,42 @@ function makeSidebar(){
         i.innerHTML = pagetitle[j];
         i.classList.add("bold");
         //console.log(location.href.split("/").slice(-1));
-        if(location.href.split("/").slice(-1) == pagenames[j]+".html"){
+        if(location.href.split("/").slice(-1) == pagenames[j]+".html")
+        {
             i.classList.add("underline");
         }
+        
         divContainer.appendChild(i);
-        divContainer.appendChild(document.createElement("br"));
-        divContainer.appendChild(document.createElement("br"));
+        //divContainer.appendChild(document.createElement("br"));
+        //divContainer.appendChild(document.createElement("br"));
+        if(location.href.split("/").slice(-1) == "beginner.html" && pagenames[j]+".html" == "beginner.html")
+        {
+            var list = document.createElement("ul");
+            list.classList.add("nobullets");
+            var litem;
+            var anchor;
+            for(var a = 0; a < beginnerids.length; a++)
+            {
+                litem = document.createElement("li");
+                anchor = document.createElement("a");
+                //anchor.href = "guides/beginner.html#" + beginnerids[a];
+                //anchor.href = "javascript:secjump()";
+                anchor.href = "javascript:secjump(\""+beginnerids[a]+"\")";
+                //anchor = document.createElement("button");
+                //anchor.addEventListener("click",function(){scrollTo(0,1200);}); 
+                anchor.innerHTML = beginnerSections[a];
+                litem.appendChild(anchor);
+                list.appendChild(litem);
+            }
+            divContainer.appendChild(list);
+        }
+        divContainer.classList.add("guidelink");
         sidenav.appendChild(divContainer);
     }
+}
+function secjump(id){
+    //scrollTo(0,y);
+    document.getElementById(id).scrollIntoView();
+    window.scrollBy(0,-64);
+    ToggleSide();
 }
