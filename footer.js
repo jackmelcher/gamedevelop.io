c96a81eab4e3c8d3df935fc2adad3dd945136686
Jack <melcher.jack@gmail.com>
2020-04-26 18:55:44 -0700

    Testing a fix for the footer in Resources. Minor changes to stylesheets, guides, and footer

--- footer.js
+++ footer.js
@@ -1,7 +1,7 @@
 var div = document.createElement("div");
 var a1 = document.createElement("a");
 var a2 = document.createElement("a");
-var a3 = document.createElement("a");
+var a3 = document.createElement("p");
 div.className = "footer";
 a1.href = "about.html";
 a1.innerHTML = "About";
@@ -9,6 +9,8 @@ div.appendChild(a1);
 a2.href = "suggestion.html";
 a2.innerHTML = "Make a Suggestion";
 div.appendChild(a2);
+var br = document.createElement("br")
+div.appendChild(br);
 a3.innerHTML = "© 2020 GameDevelop.io All rights reserved. All trademarks and registered trademarks are the property of their respective owners.";
 div.appendChild(a3);
 document.body.appendChild(div);
