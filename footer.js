70dde8602b4620163a550c690ba58f3830aac9d1
Jack <melcher.jack@gmail.com>
2020-04-29 20:34:35 -0700

    Attempted fixes for footer in resources and about pages.

--- footer.js
+++ footer.js
@@ -2,6 +2,7 @@ var div = document.createElement("div");
 var a1 = document.createElement("a");
 var a2 = document.createElement("a");
 var a3 = document.createElement("p");
+
 div.className = "footer";
 a1.href = "about.html";
 a1.innerHTML = "About";
@@ -14,12 +15,16 @@ div.appendChild(br);
 a3.innerHTML = "© 2020 GameDevelop.io All rights reserved. All trademarks and registered trademarks are the property of their respective owners.";
 div.appendChild(a3);
 document.body.appendChild(div);
-checkHeight();
-function checkHeight(){
+
+checkHeight(0);
+window.addEventListener('resize', checkHeight(1));
+document.body.addEventListener('resize', checkHeight(2));
+
+function checkHeight(a){
+    console.log(a);
+    console.log(document.body.scrollHeight+" scroll v body "+window.innerHeight);
     if(document.body.scrollHeight > window.innerHeight){
         div.style.position = "relative";
     }
     else{div.style.position = "absolute";}
 }
-window.addEventListener('resize', checkHeight);
-
