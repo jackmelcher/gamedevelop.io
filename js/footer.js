170e134ccc5d99c77cd8632fa294cba2b32a205c
Jack <melcher.jack@gmail.com>
2020-06-19 01:43:45 -0700

    Added redirects in netlify.toml, remove js footer, replaced with html footer, more updates to the beginners guide.

--- js/footer.js
+++ js/footer.js
@@ -1,23 +1,3 @@
-var div = document.querySelector('.footer');
-if(div == null){div= document.createElement("div");}
-div= document.createElement("div");
-var a1 = document.createElement("a");
-var a2 = document.createElement("a");
-var a3 = document.createElement("p");
-
-div.className = "footer";
-a1.href = "about.html";
-a1.textContent = "About";
-div.appendChild(a1);
-a2.href = "suggestion.html";
-a2.textContent = "Make a Suggestion";
-div.appendChild(a2);
-var br = document.createElement("br")
-div.appendChild(br);
-a3.textContent = "© 2020 GameDevelop.io All rights reserved. All trademarks and registered trademarks are the property of their respective owners.";
-div.appendChild(a3);
-document.body.appendChild(div);
-
 checkHeight();
 window.addEventListener('resize', checkHeight());
 document.body.addEventListener('resize',checkHeight());
