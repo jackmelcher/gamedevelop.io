bf84e012b699f8599a41c683d0999be5f0dfc102
Jack <melcher.jack@gmail.com>
2020-07-11 23:54:00 -0700

    Added settings menu in the home page and updated the style sheets. Fixed some bugs

--- js/theme.js
+++ js/theme.js
@@ -1,12 +1,31 @@
+// The Settings button
+var setting = document.getElementsByClassName("settingsnav")[0];
+
+function ToggleSettings()
+{
+    if(setting.style.display == "none" || setting.style.display == "")
+    {
+        setting.style.display = "block";
+    }
+    else
+    {
+        setting.style.display = "none";
+    }
+}
+
+// Theme stuff
+var themetext = document.getElementById("themetext");
 function toggleTheme () {
     const htmlTag = document.getElementsByTagName("html")[0]
     if (htmlTag.hasAttribute("data-theme")) {
-        htmlTag.removeAttribute("data-theme")
-        localStorage.removeItem("site-theme")
+        htmlTag.removeAttribute("data-theme");
+        localStorage.removeItem("site-theme");
+        themetext.textContent = "Light Mode:";
     }
     else{
         htmlTag.setAttribute("data-theme", "dark");
         localStorage.setItem("site-theme", "dark");
+        themetext.textContent = "Dark Mode:";
     }
 }
 
@@ -22,3 +41,4 @@ function applyInitialTheme () {
     }
 }
 applyInitialTheme();
+
