a1c5c412e5f3dd26f803aa5a6a157e33df635eb9
Jack Melcher <melcher.jack@gmail.com>
2020-10-12 22:18:03 -0700

    Updated pop, engine, sdk, audio, and asset tables. Added crisp-edges image rendering to resource images

--- functions/hello.js
+++
@@ -1,7 +0,0 @@
-exports.handler = function(event, context, callback) {
-    // your server-side functionality
-    callback(null, {
-        statusCode: 200,
-        body: "Hello, World"
-        });
-}
